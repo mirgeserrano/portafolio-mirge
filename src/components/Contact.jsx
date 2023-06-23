d5bc3ec7a40057082b6ff463b4441bd5aa3e15c5
mirge <mirgeserrano@gmail.com>
2023-06-23 09:34:50 -0400

    cmabios 23/06/23

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,10 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { useForm } from "../hook/useForm";
 
-//import portafolioApi from "../api/portafolioApi";
+//import contactImg from "../assets/img/contact-img.svg";
+import contactImg from "../assets/img/prueba.png";
+import TrackVisibility from "react-on-screen";
+
 import Swal from "sweetalert2";
 import portafolioApi from "../api/portafolioApi";
 
@@ -23,7 +26,6 @@ export const Contact = () => {
     const correoDestino = email;
     const contenido = message;
     const asunto = firstName;
-    //console.log({ firstName, lastName, email, phone, message, onInputChange });
     try {
       const { data } = await portafolioApi.post("form", {
         correoDestino,
@@ -42,58 +44,86 @@ export const Contact = () => {
   return (
     <section className="contact" id="connect">
       <Container>
-        <form className="align-items-center" onSubmit={onSumitd}>
-          <Row>
-            <Col size={12} sm={6} className="px-1">
-              <input
-                type="text"
-                placeholder="Nombre"
-                name="firstName"
-                value={firstName}
-                onChange={onInputChange}
-              />
-            </Col>
-            <Col size={12} sm={6} className="px-1">
-              <input
-                type="text"
-                placeholder="Apellido"
-                name="lastName"
-                value={lastName}
-                onChange={onInputChange}
-              />
-            </Col>
-            <Col size={12} sm={6} className="px-1">
-              <input
-                type="email"
-                placeholder="Correo"
-                name="email"
-                value={email}
-                onChange={onInputChange}
-              />
-            </Col>
-            <Col size={12} sm={6} className="px-1">
-              <input
-                type="tel"
-                placeholder="Phone"
-                name="phone"
-                value={phone}
-                onChange={onInputChange}
-              />
-            </Col>
-            <Col size={12} className="px-1">
-              <textarea
-                rows="6"
-                placeholder="Message"
-                name="message"
-                value={message}
-                onChange={onInputChange}
-              ></textarea>
-              <button type="submit">
-                <span>Enviar</span>
-              </button>
-            </Col>
-          </Row>
-        </form>
+        <Row className="align-items-center">
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <img
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                  src={contactImg}
+                  alt="Contact Us"
+                />
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Contactamé</h2>
+                  <form className="align-items-center" onSubmit={onSumitd}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="text"
+                          placeholder="Nombre"
+                          name="firstName"
+                          value={firstName}
+                          onChange={onInputChange}
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="text"
+                          placeholder="Apellido"
+                          name="lastName"
+                          value={lastName}
+                          onChange={onInputChange}
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="email"
+                          placeholder="Correo"
+                          name="email"
+                          value={email}
+                          onChange={onInputChange}
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="tel"
+                          placeholder="Phone"
+                          name="phone"
+                          value={phone}
+                          onChange={onInputChange}
+                        />
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea
+                          rows="6"
+                          placeholder="Message"
+                          name="message"
+                          value={message}
+                          onChange={onInputChange}
+                        ></textarea>
+                        <button type="submit">
+                          <span>Enviar</span>
+                        </button>
+                      </Col>
+                    </Row>
+                  </form>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
       </Container>
     </section>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -72,7 +72,7 @@ export const NavBar = () => {
 
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#">
+              <a href="https://www.linkedin.com/in/mirgelys-serrano-b232a4106/">
                 <img src={navIcon1} alt="" />
               </a>
               <a href="https://www.facebook.com/Mirgelyserrano">
